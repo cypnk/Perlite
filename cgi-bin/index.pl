9b75a74367cc27714947ea113b6d49245f3d0e9c
Rustic Cyberpunk <32854305+cypnk@users.noreply.github.com>
2025-03-19 04:36:36 -0400

    Moved subroutines to FileUtil.pm

--- cgi-bin/index.pl
+++ cgi-bin/index.pl
@@ -31,6 +31,7 @@ use lib '../lib';
 use Perlite::Filter;
 use Perlite::Util;
 use Perlite::Format;
+use Perlite::FileUtil;
 
 BEGIN {
 	# Remove in production
@@ -67,21 +68,6 @@ use constant {
 
 
 
-
-# Formatted date time helper
-sub timestamp {
-	my @time	= localtime();
-	
-	return sprintf(
-		"%04d%02d%02d_%02d%02d%02d",
-		$time[5] + 1900,	# Year
-		$time[4] + 1,		# Month (1-based)
-		$time[3],		# Day
-		$time[2],		# Hour
-		$time[1],		# Minute
-		$time[0]		# Second
-	);
-}
 
 # Get raw __DATA__ content as text
 sub getRawData {
@@ -298,286 +284,6 @@ foreach my $var (sort(keys(%ENV))) {
 
 
 
-# IO Helpers
-
-
-
-
-# Convert to a valid file or directory path
-sub filterPath {
-	my ( $path, $ns ) = @_;
-	
-	# Define reserved characters
-	state @reserved	= qw( : * ? " < > | ; );
-	
-	# New filter characters?
-	if ( $ns ) {
-		@reserved = @{ mergeArrayUnique( \@reserved, $ns ) };
-	}
-	
-	my $chars	= join( '', map { quotemeta( $_ ) } @reserved );
-	$path		=~ s/[$chars]//g;
-	$path		= unifySpaces( $path );
-	
-	# Convert relative path to absolute path if needed
-	if ( !file_name_is_absolute( $path ) && $path =~ /\S/ ) {
-		$path = rel2abs( $path );
-	}
-	
-	# Canonical filter
-	return canonpath( $path );
-}
-
-# Convert to cross-platform safe filename
-sub filterFileName {
-	my ( $fname, $ns ) = @_;
-	state @reserved = 
-	qw(
-		CON PRN AUX NUL COM1 COM2 COM3 COM4 COM5 COM6 COM7 COM8 COM9 \
-		LPT1 LPT2 LPT3 LPT4 LPT5 LPT6 LPT7 LPT8 LPT9
-	);
-	
-	# Append to reserved list?
-	if ( $ns ) {
-		@reserved = @{ mergeArrayUnique( \@reserved, $ns ) };
-	}
-	
-	# Basic filtering
-	$fname = filterPath( $fname );
-	$fname =~ s/[\/\\]/_/g;
-	$fname =~ s/^./_/;
-	
-	# Reserved filtering
-	for my $res ( @reserved ) {
-		if ( lc( $fname ) eq lc( $res ) ) {
-			$fname	= "_$fname";
-			last;
-		}
-	}
-	
-	# Maximum file name length
-	my $fnlimit	= setting( 'file_name_limit', 'int', 255 );
-	return substr( $fname, 0, $fnlimit ); 
-}
-
-# Relative storage directory
-sub storage {
-	my ( $path ) = @_;
-	state $dir;
-	
-	unless ( defined $dir ) {
-		$dir = pacify( STORAGE_DIR );
-		if ( $dir eq '' ) {
-			die "Storage directory is empty";
-		}
-		
-		$dir = filterPath( $dir );
-		unless ( -d $dir && -r $dir && -w $dir ) {
-			die "Storage directory is not accessible";
-		}
-	}
-	
-	$path	= pacify( $path );
-	
-	# Remove leading slashes and spaces, if any, and double dots
-	$path	=~ s/^[\s\/]+//;
-	$path	=~ s/\.{2,}/\./g;
-	
-	return catfile( $dir, $path );
-}
-
-# Rename duplicate files until the filename doesn't conflict
-sub dupRename {
-	my ( $dir, $fname, $path ) = @_;
-	
-	my ( $base, $ext ) = fileparse( $fname, qr/\.[^.]*/ );
-	my $i	= 1;
-	
-	# Keep modifying until file name doesn't exist
-	while ( -e $path ) {
-		$path	= catfile( $dir, "${base} ($i)$ext" );
-                $i++;
-	}
-	
-	return $path;
-}
-
-# File lock/unlock helper
-sub fileLock {
-	my ( $fname, $ltype ) = @_;
-	
-	$fname	= unifySpaces( $fname );
-	unless ( $fname =~ /^(.*)$/ ) {
-		# File name failure
-		return 0;
-	}
-	$fname	= canonpath( $1 );
-	
-	# Lockfile name
-	my $fl	= "$fname.lock___";
-	
-	# Default to removing lock
-	$ltype	//= 0;
-	
-	# Remove lock
-	if ( $ltype == 0 ) {
-		# No lock
-		if ( ! -f $fl ) {
-			return 1;
-		}
-		unlink( $fl ) or return 0;
-		return 1; # Lock removed
-	}
-	
-	my $tries	= setting( 'lock_tries', 'int', 4 );
-	while ( not sysopen ( my $fh, $fl, O_WRONLY | O_EXCL | O_CREAT ) ) {
-		if ( $tries == 0 ) {
-			return 0;
-		}
-		
-		# Couldn't open lock even without lock file existing?
-		if ( $! && $! != EEXIST ) {
-			return 0; # Lock failed
-		}
-		
-		$tries--;
-		sleep 0.1;
-	}
-	
-	# Lock acquired
-	return 1;
-}
-
-# Search path(s) for files by given pattern
-sub fileList {
-	my ( $dir, $fref, $pattern ) = @_;
-	unless ( -d $dir ) {
-		return;
-	}
-	
-	$pattern	= 
-	quotemeta( $pattern ) unless ref( $pattern ) eq 'Regexp';
-	
-	find( sub {
-		no warnings 'once';
-		push( @{$fref}, $File::Find::name ) if ( $_ =~ $pattern );
-	}, $dir );
-}
-
-# Get file contents
-sub fileRead {
-	my ( $file ) = @_;
-	my $out	= '';
-	
-	$file	=~ /^(.*)$/ and $file = $1;
-	
-	open ( my $lines, '<:encoding(UTF-8)', $file ) or exit 1;
-	while ( <$lines> ) {
-		$out .= $_;
-	}
-	
-	close ( $lines );
-	return $out;
-}
-
-# Write contents to file
-sub fileWrite {
-	my ( $file, $data ) = @_;
-	
-	$file	=~ /^(.*)$/ and $file = $1;
-	
-	open ( my $lines, '>:encoding(UTF-8)', $file ) or exit 1;
-	print $lines $data;
-	
-	close ( $lines );
-}
-
-# Search directory for words
-sub searchFiles {
-	my ( $dir, $words, $ext, $page, $limit )	= @_;
-	
-	unless ( -d $dir ) {
-		return ();
-	}
-	
-	my $pattern	= join( '|', map { quotemeta } @$ext );
-	$pattern	= qr/\Q$pattern\E$/i;
-	
-	$limit		//= 10;
-	$page		//= 1;
-	
-	my $offset	= $limit * $page - 1;
-	
-	my @files;
-	fileList( $dir, \@files, $pattern );
-	
-	@files = sort( @files );
-	
-	my @items	= ();
-	my $count	= 0;
-	my $found	= 0;
-	
-	foreach my $fpath ( @files ) {
-		if ( @items >= $limit ) {
-			last;
-		}
-		
-		open ( my $fh, '<:encoding(UTF-8)', $fpath ) or next;
-		
-		# Line-by line search
-		while ( my $line = <$fh> ) {
-			# Iterate through search terms
-			foreach my $word ( @$words ) {
-				if ( $line =~ /\b\Q$word\E\b/i) {
-					$found = 1;
-					last;
-				}
-			}
-			
-			# Skip rest of the lines
-			if ( $found ) {
-				last;
-			}
-		}
-		
-		close( $fh );
-		
-		if ( $found ) {
-			$count++;
-			if ( $count > $offset ) {
-				push( @items, $fpath );
-			}
-			$found	= 0;
-		}
-	}
-	
-	return @items;
-}
-
-# Save text to file
-sub fileSave {
-	my ( $path, $data ) = @_;
-	
-	$path = storage( $path );
-	
-	# If file exists, create a backup
-	if ( -e $path ) {
-		my $bkp	= $path . '_backup_' . timestamp();
-		copy( $path, $bkp ) or die "Failed creating backup";
-	}
-	
-	fileWrite( $path, $data );
-}
-
-# Send output buffer to client and enable auto flush
-sub startFlush() {
-	STDOUT->flush();
-	STDOUT->autoflush( 1 );
-}
-
-
-
-
 # Configuration settings
 
 
@@ -639,7 +345,7 @@ sub config {
 	
 	# Default config
 	my $cfile		= CONFIG_FILE;
-	my $conf		= fileRead( storage( $cfile ) );
+	my $conf		= fileRead( storage( $cfile, STORAGE_DIR ) );
 	if ( $conf ne '' ) {
 		%settings	= jsonDecode( $conf );
 	}
@@ -655,7 +361,7 @@ sub config {
 	# Find realm specific config, if given, and merge to core
 	if ( $realm ne '' ) {
 		my $rfile	= catfile( $realm, CONFIG_FILE );
-		my $rconf	= fileRead( storage( $rfile ) );
+		my $rconf	= fileRead( storage( $rfile, STORAGE_DIR ) );
 		if ( $rconf ne '' ) {
 			my %nconfig	= jsonDecode( $rconf );
 			if ( keys %nconfig ) {
@@ -781,7 +487,7 @@ sub siteRealm {
 	sendBadRequest() if ( $realm eq '' );
 	
 	# Check for reqested realm, if it exists
-	my $dir = storage( catfile( 'sites', $realm ) );
+	my $dir = storage( catfile( 'sites', $realm ), STORAGE_DIR );
 	if ( ! -d $dir ) {
 		sendBadRequest();
 	}
@@ -940,7 +646,7 @@ sub formDataStream {
 	
 	my ( $tfh, $tfn )	= 
 	tempfile(
-		DIR	=> storage( UPLOADS ), 
+		DIR	=> storage( UPLOADS, STORAGE_DIR ), 
 		SUFFIX	=> '.tmp' 
 	);
 	
@@ -1044,7 +750,7 @@ sub formDataSegment {
 	/ix;
 
 	# File uploads and form handling temp file directory
-	my $dir		= storage( UPLOADS );
+	my $dir		= storage( UPLOADS, STORAGE_DIR );
 	my %err;
 	
 	foreach my $part ( @segs ) {
@@ -1503,7 +1209,7 @@ sub sessionRead {
 	
 	$id		= sessionCleanID( $id );
 	
-	my $sfile	= storage( catfile( SESSION_DIR, $id ) );
+	my $sfile	= storage( catfile( SESSION_DIR, $id ), STORAGE_DIR );
 	my $data	= -f $sfile ? fileRead( $sfile ) : '';
 	
 	return $data;
@@ -1564,7 +1270,7 @@ sub sessionDestroy {
 	my ( $id ) = @_;
 	$id		= sessionCleanID( $id );
 	
-	my $sfile	= storage( catfile( SESSION_DIR, $id ) );
+	my $sfile	= storage( catfile( SESSION_DIR, $id ), STORAGE_DIR );
 	if ( -f $sfile ) {
 		unlink ( $sfile );
 	}
@@ -1572,7 +1278,7 @@ sub sessionDestroy {
 
 # Garbage collection
 sub sessionGC {
-	my $sdir	= storage( SESSION_DIR );
+	my $sdir	= storage( SESSION_DIR, STORAGE_DIR );
 	my $ntime	= time();
 	
 	# Time between cleaning up old cookies
@@ -1616,12 +1322,12 @@ sub sessionWriteClose {
 		return;
 	}
 	
-	my $sdir	= storage( SESSION_DIR );
+	my $sdir	= storage( SESSION_DIR, STORAGE_DIR );
 	unless ( -d $sdir ) {
 		mkdir( $sdir, 0644 );
 	}
 	
-	my $sfile	= storage( catfile( SESSION_DIR, sessionID() ) );
+	my $sfile	= storage( catfile( SESSION_DIR, sessionID() ), STORAGE_DIR );
 	fileWrite( $sfile, encode_json( \%data ) );
 	
 	$written = 1;
@@ -1890,8 +1596,8 @@ sub sendErrorResponse {
 	}
 	
 	# Try to send the realm-specific response file, if it exists
-	my $tpl		= storage( catfile( 'sites', $realm, 'errors', "$code.html" ) );
-	my $ctpl	= storage( catfile( 'errors', "$code.html" ) );
+	my $tpl		= storage( catfile( 'sites', $realm, 'errors', "$code.html" ), STORAGE_DIR );
+	my $ctpl	= storage( catfile( 'errors', "$code.html" ), STORAGE_DIR );
 	if ( -f $tpl ) {
 		preamble( 0, 1 );
 		render( $tpl );
@@ -3109,7 +2815,7 @@ sub getPassword {
 	
 	my $pass = '';
 	
-	my $file = storage( catfile( $realm, USER_FILE ) );
+	my $file = storage( catfile( $realm, USER_FILE ), STORAGE_DIR );
 	
 	open( my $lines, '<:encoding(UTF-8)', $file ) or exit 1;
 	while ( <$lines> ) {
@@ -3135,7 +2841,7 @@ sub savePassword {
 	# Username with matching password
 	my $npass	= "$user	$pass\n";
 	
-	my $ifile	= storage( catfile( $realm, USER_FILE ) );
+	my $ifile	= storage( catfile( $realm, USER_FILE ), STORAGE_DIR );
 	
 	# Try to get a lock for this user file
 	fileLock( $ifile, 1 ) or exit 1;
@@ -3148,7 +2854,7 @@ sub savePassword {
 	
 	open( INF, '<:encoding(UTF-8)', $ifile ) or exit 1;
 	
-	my $ofile	= storage( catfile( $realm, USER_FILE . '.new' ) );
+	my $ofile	= storage( catfile( $realm, USER_FILE . '.new' ), STORAGE_DIR );
 	open( ONF, '>:encoding(UTF-8)', $ofile ) or exit 1;
 	
 	my $found	= 0;
@@ -3355,14 +3061,16 @@ sub viewStatic {
 	
 	my $loc	= 
 	storage( 
-		catfile( 'sites', $realm, 'static', $tree, $file ) 
+		catfile( 'sites', $realm, 'static', $tree, $file ), 
+		STORAGE_DIR 
 	);
 	
 	if ( ! -f $loc ) {
 		# Fallback to uploads path
 		$loc = 
 		storage( 
-			catfile( 'sites', $realm, UPLOADS, $tree, $file ) 
+			catfile( 'sites', $realm, UPLOADS, $tree, $file ), 
+			STORAGE_DIR 
 		);
 	}
 	
@@ -3407,7 +3115,7 @@ sub viewHome {
 	);
 	
 	
-	my $tpl = storage( catfile( 'sites', $realm, 'pages', 'home.html' ) );
+	my $tpl = storage( catfile( 'sites', $realm, 'pages', 'home.html' ), STORAGE_DIR );
 	
 	print render( $tpl, 'tpl_page', \%data );
 	exit;
