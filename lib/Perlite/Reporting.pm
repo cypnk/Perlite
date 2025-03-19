8bda009e7bded60ed3a3c2b5bab492d91d5afea7
Rustic Cyberpunk <32854305+cypnk@users.noreply.github.com>
2025-03-19 06:11:56 -0400

    Added scrubber to remove sensitive data

--- lib/Perlite/Reporting.pm
+++ lib/Perlite/Reporting.pm
@@ -10,7 +10,7 @@ use Exporter qw( import );
 use Perlite::Filter qw( unifySpaces );
 use Perlite::FileUtil qw( filterPath );
 
-our @EXPORT_OK	= qw( callerTrace report hasErrors );
+our @EXPORT_OK	= qw( callerTrace report cleanMsg hasErrors );
 
 
 # Helper to find nested caller subroutine details for debugging, logging etc...
@@ -66,6 +66,26 @@ sub report {
 	}
 	
 	return "${msg} ( $trace\n )";
+}
+
+# Remove sensitive data from messages
+sub cleanMsg {
+	my ( $self, $msg, $private ) = @_;
+	
+	return '' unless defined $msg;
+	
+	$private	= [] unless defined $private && ref $private eq 'ARRAY';
+	
+	my @clean	= grep { defined && $_ ne '' } @{$private};
+	
+	my $pat	= join( '|', map { quotemeta $_ } @clean );
+	return $msg unless $pat;
+	
+	my $rx	= qr/(?:$pat)/i;
+	my $out = $msg;
+	$out =~ s/$rx/REDACTED/gi;
+	
+	return $out;
 }
 
 # Check if hash has an 'error' key set and is not 0
