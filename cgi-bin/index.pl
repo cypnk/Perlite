ed5c70e2abea83b17ca12eeab2cf6fc2e213ed06
Rustic Cyberpunk <32854305+cypnk@users.noreply.github.com>
2025-03-19 05:28:04 -0400

    Moved subroutines to Reporting.pm

--- cgi-bin/index.pl
+++ cgi-bin/index.pl
@@ -32,6 +32,7 @@ use Perlite::Filter;
 use Perlite::Util;
 use Perlite::Format;
 use Perlite::FileUtil;
+use Perlite::Reporting;
 
 BEGIN {
 	# Remove in production
@@ -204,81 +205,6 @@ sub mimeList {
 		return {};
 	}
 	return %mime_list;
-}
-
-# Helper to find nested caller subroutine details for debugging, logging etc...
-sub callerTrace {
-	my ( $max_depth, $filter )	= @_;
-	
-	my @callers;
-	my $depth		= 0;
-	
-	# Presets
-	$max_depth		= 20 
-		unless defined( $max_depth ) && $max_depth =~ /^\d+$/;
-	
-	$filter			= {} if ref( $filter ) ne 'HASH';
-	$filter->{exclude}	= [] 
-		unless defined( $filter->{exclude} ) && 
-			ref( $filter->{exclude} ) ne 'ARRAY';
-	
-	while ( my $info = caller( $depth ) ) {
-		last if ( $max_depth > 0 && $depth >= $max_depth );
-		next if grep { $_ eq $info[0] } @{$filter->{exclude}};
-		
-		push( @callers, {
-			pkg	=> $info[0] // 'Unknown',
-			fname	=> $info[1] // 'Unknown',
-			line	=> $info[2] // 'Unknown',
-			func	=> $info[3] // 'Unknown',
-		} );
-		$depth++;
-	}
-	
-	return @callers;
-}
-
-# Error and message report formatting helper
-sub report {
-	my ( $msg, $depth )	= @_;
-	
-	$msg	//= 'Empty message';
-	$msg	= unifySpaces( $msg );
-	$depth	//= 1;
-	
-	my ( $pkg, $fname, $line, $func ) = caller( $depth );
-	return "${msg} ( No caller info at depth ${depth} )" 
-		unless defined $pkg; 
-	
-	$fname	= filterPath( $fname );
-	
-	return 
-	"${msg} ( Package: ${pkg}, File: ${fname}, " . 
-		"Subroutine: ${func}, Line: ${line} )";
-}
-
-# Check if hash has an 'error' key set and is not 0
-sub hasErrors {
-	my ( $ref )	= @_;
-	
-	return 
-	defined( $ref->{error} ) && ( 
-		( $ref->{error} eq 'HASH' && keys %{ $ref->{error} } ) || 
-		$ref->{error}
-	) ? 1 : 0;
-}
-
-
-##
-##  printenv -- demo CGI program which just prints its environment
-##
-
-print "Content-type: text/plain; charset=iso-8859-1\n\n";
-foreach my $var (sort(keys(%ENV))) {
-	my $val = $ENV{$var};
-	$val =~ s|\n|\\n|g;
-	$val =~ s|"|\\"|g;
-	print "${var}=\"${val}\"\n";
 }
 
 
