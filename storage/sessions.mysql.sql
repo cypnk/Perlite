cfcb151b8de541ca0669ff220629f257cc6a191e
Rustic Cyberpunk <32854305+cypnk@users.noreply.github.com>
2025-03-31 09:06:37 -0400

    Added preparation for filtering

--- storage/sessions.mysql.sql
+++ storage/sessions.mysql.sql
@@ -21,8 +21,24 @@ CREATE INDEX idx_session_site ON sessions( basename );-- --
 CREATE INDEX idx_session_ip ON sessions( session_ip );-- --
 CREATE INDEX idx_session_created ON sessions( created DESC );-- --
 CREATE INDEX idx_session_updated ON sessions( updated DESC );-- --
-CREATE INDEX idx_session_expires ON sessions( updated ASC );-- --
+CREATE INDEX idx_session_expires ON sessions( expires ASC );-- --
 
+CREATE VIEW ip_view AS
+SELECT 
+	id, basename, session_id, session_ip, created, updated, expires, 
+	CASE 
+		WHEN session_ip REGEXP '^[0-9]{1,3}(\\.[0-9]{1,3}){3}$' THEN 'IPv4' 
+		WHEN session_ip REGEXP '^([0-9a-fA-F]{1,4}:){7}[0-9a-fA-F]{1,4}$' THEN 'IPv6' 
+		WHEN session_ip REGEXP '^(([0-9a-fA-F]{1,4}:){1,7}[0-9a-fA-F]{1,4}|::)$' THEN 'IPv6' 
+		ELSE 'Unknown'
+	END AS ip_type
+FROM sessions;-- --
+
+CREATE TABLE user_agents (
+	session_id VARCHAR( 50 ) PRIMARY KEY,
+	session_ua TEXT NOT NULL,
+	FULLTEXT ( session_ua )
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_520_ci;-- --
 
 DELIMITER $$
 CREATE TRIGGER session_before_insert BEFORE INSERT ON sessions FOR EACH ROW
